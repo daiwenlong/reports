79062787cb4ea14ba1b75dd8f925daec9c263585
daiwenlong <dwl0208@163.com>
2017-11-13 20:46:17 +0800

    框架

---
+++ src/main/java/com/cn/hnust/dao/IUserDao.java
@@ -0,0 +1,17 @@
+package com.cn.hnust.dao;
+
+import com.cn.hnust.pojo.User;
+
+public interface IUserDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ src/main/java/com/cn/hnust/service/IUserService.java
@@ -0,0 +1,7 @@
+package com.cn.hnust.service;
+
+import com.cn.hnust.pojo.User;
+
+public interface IUserService {
+	public User getUserById(int userId);
+}
